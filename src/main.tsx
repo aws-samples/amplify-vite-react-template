9ddef4dd2e964880483548d211f04e0988ab6020
Jay Raval <jay.ambrish.raval@gmail.com>
2024-04-18 14:59:13 -0700

    🚀

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Amplify } from "aws-amplify";
+import amplifyConfig from "../amplifyconfiguration.json";
+
+Amplify.configure(amplifyConfig);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
