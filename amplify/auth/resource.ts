b327ac4fff39a1a04a24027c7de9bd99ccebb0cc
Gao Zhiyu <z.gao@crimsoneducation.org>
2025-05-28 11:33:28 +1200

    initail commit

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,21 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth, secret } from '@aws-amplify/backend';
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
 export const auth = defineAuth({
   loginWith: {
     email: true,
-  },
+    externalProviders: {
+      google: {
+        clientId: secret('GOOGLE_CLIENT_ID'),
+        clientSecret: secret('GOOGLE_CLIENT_SECRET'),
+        attributeMapping: {
+          email: 'email'
+        },
+      },
+      callbackUrls: [
+        'http://localhost:3000/profile',
+        'https://mywebsite.com/profile'
+      ],
+      logoutUrls: ['http://localhost:3000/', 'https://mywebsite.com'],
+    }
+  }
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,30 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-
-const client = generateClient<Schema>();
+import { useEffect } from "react";
+import { useAuthenticator } from '@aws-amplify/ui-react';
+import { fetchAuthSession } from '@aws-amplify/auth';
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
+  const { user, signOut } = useAuthenticator();
+  
   useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
+    async function getIDToken() {
+      const session = await fetchAuthSession();
+      const token = session?.tokens?.idToken;
+      console.log(token);
+      return token; 
+    }
+    getIDToken();
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
+  }, []);
 
   return (
     <main>
+      <h1>{JSON.stringify(user)}</h1>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
+      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
+
 
 export default App;
