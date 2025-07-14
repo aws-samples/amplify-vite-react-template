ea1d1e182006608d42f4623f28d48ab3c63e5b9b
“janeqqrul” <jan.karolewski1b@gmail.com>
2025-07-14 20:17:49 +0200

    Add google auth

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,4 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth, secret } from '@aws-amplify/backend';
 
 /**
  * Define and configure your auth resource
@@ -7,5 +7,15 @@ import { defineAuth } from '@aws-amplify/backend';
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    externalProviders: {
+      google: {
+        clientId: secret('GOOGLE_CLIENT_ID'),
+        clientSecret: secret('GOOGLE_CLIENT_SECRET')
+      },
+      callbackUrls: [
+        'http://localhost:3000/profile',
+      ],
+      logoutUrls: ['http://localhost:3000/'],
+    }
   },
 });
