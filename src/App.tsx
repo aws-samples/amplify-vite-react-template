dbafeeb473a63b28c918564d2afef9324916cb4a
Alex_Thornberry <Alex.Thornberry@thomsonreuters.com>
2025-09-30 11:45:20 +0100

    added navbar, sidebar and appshell

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import NavBar from "./components/NavBar/NavBar.tsx";
+import SideNav from "./components/SideNav/SideNav.tsx";
 
-const client = generateClient<Schema>();
+// const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
+    // client.models.Todo.observeQuery().subscribe({
+    //   next: (data) => setTodos([...data.items]),
+    // });
   }, []);
 
   function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
+    // client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
+
+  
   return (
-    <main>
+    <>
+    <NavBar />
+    <SideNav />
+      <div className="app-shell">
+        <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
@@ -34,6 +42,8 @@ function App() {
         </a>
       </div>
     </main>
+    </div>
+    </>
   );
 }
 
